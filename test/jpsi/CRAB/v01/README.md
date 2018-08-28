b41bd8707a122d62ee3ea765e67e0f055c7b59ac
Kyoengpil Lee <kplee@cern.ch>
2018-08-28 20:45:36 +0200

    configs. for 101X submission

---
+++ test/jpsi/CRAB/v01/README.md
@@ -0,0 +1,41 @@
+### Dataset
+
+* [DAS link](https://cmsweb.cern.ch/das/request?view=list&limit=50&instance=prod%2Fglobal&input=%2FCharmonium%2FRun2018*-PromptReco-v*%2FAOD)
+
+```
+/Charmonium/Run2018A-PromptReco-v1/AOD
+/Charmonium/Run2018A-PromptReco-v2/AOD 
+/Charmonium/Run2018A-PromptReco-v3/AOD
+/Charmonium/Run2018B-PromptReco-v1/AOD
+/Charmonium/Run2018B-PromptReco-v2/AOD
+/Charmonium/Run2018C-PromptReco-v1/AOD
+/Charmonium/Run2018C-PromptReco-v2/AOD
+/Charmonium/Run2018C-PromptReco-v3/AOD
+/Charmonium/Run2018D-PromptReco-v2/AOD
+```
+
+* Global tags
+  * 101X_dataRun2_Prompt_v9
+    * RunA-v1, v2
+  * 101X_dataRun2_Prompt_v10
+    * RunA-v3, RunB-v1
+  * 101X_dataRun2_Prompt_v11
+    * RunB-v2, RunC-v1,v2,v3
+  * RunD, v2: 102X_dataRun2_Prompt_v4
+
+
+
+* The latest CMSSW
+  * CMSSW_10_1_9
+    * RunC-v3 and below
+  * CMSSW_10_2_1
+    * RunD-v2
+
+### JSON
+
+```
+/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions18/13TeV/PromptReco/Cert_314472-321221_13TeV_PromptReco_Collisions18_JSON.txt
+```
+
+
+
---
+++ test/jpsi/CRAB/v01/crabConfig_DATA_101X.py
@@ -0,0 +1,77 @@
+from CRABClient.UserUtilities import config, getUsernameFromSiteDB
+config = config()
+
+config.General.requestName = ''
+config.General.workArea = 'CRABDir'
+
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName = ''
+# config.JobType.numCores = 4
+# config.JobType.maxMemoryMB = 2500
+# config.JobType.maxJobRuntimeMin = 2000
+
+config.Data.inputDataset = ''
+# config.Data.useParent = True
+
+config.Data.inputDBS = 'global'
+config.Data.splitting = 'Automatic'
+# config.Data.unitsPerJob = 3
+config.Data.outLFNDirBase = '/store/user/%s/' % (getUsernameFromSiteDB())
+config.Data.publication = False
+# config.Data.ignoreLocality = True
+# config.Site.whitelist = ['T2_KR_*', 'T2_US_*', 'T2_IT_*', 'T3_IT_*'] # -- mandatory for ignoreLocality option
+# config.Site.storageSite = 'T3_KR_KISTI'
+config.Site.storageSite = 'T2_KR_KNU'
+
+
+config.Data.lumiMask = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions18/13TeV/PromptReco/Cert_314472-321221_13TeV_PromptReco_Collisions18_JSON.txt'
+# config.Data.runRange = '305636-305636'
+
+version = '_v20180828_'
+# 'MultiCRAB' part
+if __name__ == '__main__':
+    
+    from CRABAPI.RawCommand import crabCommand
+
+    # -- Run2018A
+    config.General.requestName = 'TnPTreeJPsi'+version+'SingleMuon_Run2018Av1_GoldenJSON'
+    config.Data.inputDataset = '/Charmonium/Run2018A-PromptReco-v1/AOD'
+    config.JobType.psetName = '../../tp_from_aod_simple_Data_CtoF_101X_v9.py'
+    crabCommand('submit', config = config)
+
+    config.General.requestName = 'TnPTreeJPsi'+version+'SingleMuon_Run2018Av2_GoldenJSON'
+    config.Data.inputDataset = '/Charmonium/Run2018A-PromptReco-v2/AOD'
+    config.JobType.psetName = '../../tp_from_aod_simple_Data_CtoF_101X_v9.py'
+    crabCommand('submit', config = config)
+
+    config.General.requestName = 'TnPTreeJPsi'+version+'SingleMuon_Run2018Av3_GoldenJSON'
+    config.Data.inputDataset = '/Charmonium/Run2018A-PromptReco-v3/AOD'
+    config.JobType.psetName = '../../tp_from_aod_simple_Data_CtoF_101X_v10.py'
+    crabCommand('submit', config = config)
+
+    # -- Run2018B
+    config.General.requestName = 'TnPTreeJPsi'+version+'SingleMuon_Run2018Bv1_GoldenJSON'
+    config.Data.inputDataset = '/Charmonium/Run2018B-PromptReco-v1/AOD'
+    config.JobType.psetName = '../../tp_from_aod_simple_Data_CtoF_101X_v10.py'
+    crabCommand('submit', config = config)
+
+    config.General.requestName = 'TnPTreeJPsi'+version+'SingleMuon_Run2018Bv2_GoldenJSON'
+    config.Data.inputDataset = '/Charmonium/Run2018B-PromptReco-v2/AOD'
+    config.JobType.psetName = '../../tp_from_aod_simple_Data_CtoF_101X_v11.py'
+    crabCommand('submit', config = config)
+
+    # -- Run2018C
+    config.General.requestName = 'TnPTreeJPsi'+version+'SingleMuon_Run2018Cv1_GoldenJSON'
+    config.Data.inputDataset = '/Charmonium/Run2018C-PromptReco-v1/AOD'
+    config.JobType.psetName = '../../tp_from_aod_simple_Data_CtoF_101X_v11.py'
+    crabCommand('submit', config = config)
+
+    config.General.requestName = 'TnPTreeJPsi'+version+'SingleMuon_Run2018Cv2_GoldenJSON'
+    config.Data.inputDataset = '/Charmonium/Run2018C-PromptReco-v2/AOD'
+    config.JobType.psetName = '../../tp_from_aod_simple_Data_CtoF_101X_v11.py'
+    crabCommand('submit', config = config)
+
+    config.General.requestName = 'TnPTreeJPsi'+version+'SingleMuon_Run2018Cv3_GoldenJSON'
+    config.Data.inputDataset = '/Charmonium/Run2018C-PromptReco-v3/AOD'
+    config.JobType.psetName = '../../tp_from_aod_simple_Data_CtoF_101X_v11.py'
+    crabCommand('submit', config = config)
