fe6a933b4f5cee8ab413379bd152880f6a58da75
Kyoengpil Lee <kplee@cern.ch>
2018-09-03 10:31:12 +0200

    update # events for test

--- test/jpsi/tp_from_aod_simple_Data_v2.py
+++ test/jpsi/tp_from_aod_simple_Data_v2.py
@@ -10,7 +10,7 @@ process.MessageLogger.cerr.FwkReport.reportEvery = 1000
 process.source = cms.Source("PoolSource", 
     fileNames = cms.untracked.vstring(),
 )
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )    
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(100) )    
 
 process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
 process.load('Configuration.StandardSequences.MagneticField_cff')
