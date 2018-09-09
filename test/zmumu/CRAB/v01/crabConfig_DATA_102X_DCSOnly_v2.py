31729966e063aa002d578db3d7dccc6b69658f8a
Kyoengpil Lee <kplee@cern.ch>
2018-09-09 12:33:08 +0200

    more data with DSC

---
+++ test/zmumu/CRAB/v01/crabConfig_DATA_102X_DCSOnly_v2.py
@@ -0,0 +1,40 @@
+from CRABClient.UserUtilities import config, getUsernameFromSiteDB
+config = config()
+
+config.General.requestName = ''
+config.General.workArea = 'CRABDir'
+
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName = ''
+# config.JobType.numCores = 4
+# config.JobType.maxMemoryMB = 2500
+# config.JobType.maxJobRuntimeMin = 2000
+
+config.Data.inputDataset = ''
+# config.Data.useParent = True
+
+config.Data.inputDBS = 'global'
+config.Data.splitting = 'Automatic'
+# config.Data.unitsPerJob = 3
+config.Data.outLFNDirBase = '/store/user/%s/' % (getUsernameFromSiteDB())
+config.Data.publication = False
+# config.Data.ignoreLocality = True
+# config.Site.whitelist = ['T2_KR_*', 'T2_US_*', 'T2_IT_*', 'T3_IT_*'] # -- mandatory for ignoreLocality option
+# config.Site.storageSite = 'T3_KR_KISTI'
+config.Site.storageSite = 'T2_KR_KNU'
+
+config.Data.lumiMask = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions18/13TeV/DCSOnly/json_DCSONLY.txt'
+firstRun = 322069
+lastRun  = 322324
+config.Data.runRange = '%d-%d' % (firstRun, lastRun)
+
+version = '_v20180909_'
+# 'MultiCRAB' part
+if __name__ == '__main__':
+    
+    from CRABAPI.RawCommand import crabCommand
+
+    config.General.requestName = 'TnPTreeZ'+version+'SingleMuon_Run2018Dv2_DCSOnly_%d_to_%d' % (firstRun, lastRun)
+    config.Data.inputDataset = '/SingleMuon/Run2018D-PromptReco-v2/AOD'
+    config.JobType.psetName = '../../tp_from_aod_Data_102X_v4.py'
+    crabCommand('submit', config = config)
