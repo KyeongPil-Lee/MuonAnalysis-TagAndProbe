53632d3443316904b3a9d9c6c6673d33028579a3
Kyoengpil Lee <kplee@cern.ch>
2018-09-10 13:12:46 +0200

    tyop

--- test/zmumu/CRAB/v01/script_moveFromKNUToMuon.sh
+++ test/zmumu/CRAB/v01/script_moveFromKNUToMuon.sh
@@ -1,15 +1,15 @@
 #!bin/bash
 
 hadd TnPTreeZ_SingleMuon_Run2018Cv3_GoldenJSON_from319852ToEnd.root \
-/pnfs/knu.ac.kr/data/cms/store/user/kplee/crab_TnPTreeZ_v20180904_SingleMuon_Run2018Cv3_GoldenJSON_from319852ToEnd/180904_093401/0000/*.root
+/pnfs/knu.ac.kr/data/cms/store/user/kplee/SingleMuon/crab_TnPTreeZ_v20180904_SingleMuon_Run2018Cv3_GoldenJSON_from319852ToEnd/180904_093401/0000/*.root
 
 hadd TnPTreeZ_SingleMuon_Run2018Dv2_GoldenJSON_Upto321461.root \
-/pnfs/knu.ac.kr/data/cms/store/user/kplee/crab_TnPTreeZ_v20180904_SingleMuon_Run2018Dv2_GoldenJSON_Upto321461/180904_093517/0000/*.root \
-/pnfs/knu.ac.kr/data/cms/store/user/kplee/crab_TnPTreeZ_v20180904_SingleMuon_Run2018Dv2_GoldenJSON_Upto321461/180904_093517/0001/*.root
+/pnfs/knu.ac.kr/data/cms/store/user/kplee/SingleMuon/crab_TnPTreeZ_v20180904_SingleMuon_Run2018Dv2_GoldenJSON_Upto321461/180904_093517/0000/*.root \
+/pnfs/knu.ac.kr/data/cms/store/user/kplee/SingleMuon/crab_TnPTreeZ_v20180904_SingleMuon_Run2018Dv2_GoldenJSON_Upto321461/180904_093517/0001/*.root
 
 hadd TnPTreeZ_SingleMuon_Run2018Dv2_DCSOnly_321462_to_322068.root \
-/pnfs/knu.ac.kr/data/cms/store/user/kplee/crab_TnPTreeZ_v20180906_SingleMuon_Run2018Dv2_DCSOnly_321462_to_322068/180906_174333/0000/*.root \
-/pnfs/knu.ac.kr/data/cms/store/user/kplee/crab_TnPTreeZ_v20180906_SingleMuon_Run2018Dv2_DCSOnly_321462_to_322068/180906_174333/0001/*.root
+/pnfs/knu.ac.kr/data/cms/store/user/kplee/SingleMuon/crab_TnPTreeZ_v20180906_SingleMuon_Run2018Dv2_DCSOnly_321462_to_322068/180906_174333/0000/*.root \
+/pnfs/knu.ac.kr/data/cms/store/user/kplee/SingleMuon/crab_TnPTreeZ_v20180906_SingleMuon_Run2018Dv2_DCSOnly_321462_to_322068/180906_174333/0001/*.root
 
 echo "hadd: finished"
 
