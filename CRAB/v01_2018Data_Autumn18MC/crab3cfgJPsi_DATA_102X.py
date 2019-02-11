a15f37c813319d304ee6a00846e0dd4183d72c16
kplee <kplee@cern.ch>
2019-02-12 00:10:06 +0900

    update tag

--- CRAB/v01_2018Data_Autumn18MC/crab3cfgJPsi_DATA_102X.py
+++ CRAB/v01_2018Data_Autumn18MC/crab3cfgJPsi_DATA_102X.py
@@ -25,7 +25,7 @@ config.Site.storageSite = 'T2_KR_KNU'
 
 config.Data.lumiMask = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions18/13TeV/PromptReco/Cert_314472-325175_13TeV_PromptReco_Collisions18_JSON.txt'
 
-version = '_v20190211_'
+version = '_v20190212_'
 # 'MultiCRAB' part
 if __name__ == '__main__':
     
--- CRAB/v01_2018Data_Autumn18MC/crab3cfgJPsi_MC_102X.py
+++ CRAB/v01_2018Data_Autumn18MC/crab3cfgJPsi_MC_102X.py
@@ -25,7 +25,7 @@ config.Site.storageSite = 'T2_KR_KNU'
 # config.Data.runRange = '%d-%d' % (FirstRun, LastRun)
 
 
-version = '_v20190211_'
+version = '_v20190212_'
 # 'MultiCRAB' part
 if __name__ == '__main__':
     
