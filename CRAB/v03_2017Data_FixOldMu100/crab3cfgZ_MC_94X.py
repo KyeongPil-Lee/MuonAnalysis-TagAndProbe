4ee94534fcbff1fc555f38f6fb133ab0c3d6f333
kplee <kplee@cern.ch>
2019-05-09 10:46:18 +0900

    config for 94X MC

---
+++ CRAB/v03_2017Data_FixOldMu100/crab3cfgZ_MC_94X.py
@@ -0,0 +1,41 @@
+from CRABClient.UserUtilities import config, getUsernameFromSiteDB
+config = config()
+
+config.General.requestName = ''
+config.General.workArea = 'CRABDir'
+
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName = '../../test/zmumu/tp_from_aod_MC_94X_mc2017_realistic_v10.py'
+
+config.Data.inputDataset = ''
+
+config.Data.inputDBS = 'global'
+# config.Data.splitting = 'Automatic'
+config.Data.splitting = 'FileBased'
+config.Data.unitsPerJob = 10
+config.Data.outLFNDirBase = '/store/user/%s/' % (getUsernameFromSiteDB())
+config.Data.publication = False
+
+# config.Site.storageSite = 'T3_KR_KISTI'
+config.Site.storageSite = 'T2_KR_KNU'
+
+# config.Data.lumiMask = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions16/13TeV/ReReco/Final/Cert_271036-284044_13TeV_23Sep2016ReReco_Collisions16_JSON.txt'
+# FirstRun = 271036
+# LastRun = 284044
+# config.Data.runRange = '%d-%d' % (FirstRun, LastRun)
+
+
+version = '_v20190509_'
+# 'MultiCRAB' part
+if __name__ == '__main__':
+    
+    from CRABAPI.RawCommand import crabCommand
+
+    config.General.requestName = 'TnPTreeZ'+version+'94X_DYJetsToLL_M50_MadgraphMLM'
+    config.Data.inputDataset = '/DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8/RunIIFall17DRPremix-RECOSIMstep_94X_mc2017_realistic_v10-v1/AODSIM'
+    # config.Data.allowNonValidInputDataset = True # -- current status = Production
+    crabCommand('submit', config = config)
+
+    # config.General.requestName = ''
+    # config.Data.inputDataset = ''
+    # crabCommand('submit', config = config)
