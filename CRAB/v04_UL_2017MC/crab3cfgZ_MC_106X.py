8049d1456805f57e64fe913d9469784c713e5d3d
kplee <kplee@cern.ch>
2019-05-09 16:17:46 +0900

    typo

--- CRAB/v04_UL_2017MC/crab3cfgZ_MC_106X.py
+++ CRAB/v04_UL_2017MC/crab3cfgZ_MC_106X.py
@@ -32,7 +32,7 @@ if __name__ == '__main__':
     from CRABAPI.RawCommand import crabCommand
 
     config.General.requestName = 'TnPTreeZ'+version+'1060pre2_RelValZMM'
-    config.Data.inputDataset = '/RelValZMM_13UP17/CMSSW_10_6_0_pre2-PUpmx25ns_105X_mc2017_realistic_v7_ulhlt17hs_pmx-v1/FEVTDEBUGHLT '
+    config.Data.inputDataset = '/RelValZMM_13UP17/CMSSW_10_6_0_pre2-PUpmx25ns_105X_mc2017_realistic_v7_ulhlt17hs_pmx-v1/FEVTDEBUGHLT'
     config.JobType.psetName = '../../test/zmumu/tp_from_aod_MC_105X_mc2017_realistic_v7.py'
     # config.Data.allowNonValidInputDataset = True # -- current status = Production
     crabCommand('submit', config = config)
