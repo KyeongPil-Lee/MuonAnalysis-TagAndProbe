4dbbd0cf08326919e87cb76ab57019b16e4a44e9
kplee <kplee@cern.ch>
2019-06-07 18:41:47 +0900

    last job

---
+++ CRAB/v02_2016DataMC_newHighPtID/genScript_mergeAndTrasnferToEOS_v6.py
@@ -0,0 +1,27 @@
+from MuonAnalysis.TagAndProbe.TransferToEOSTool import *
+
+tool = TransferToEOSTool()
+# -- common settings
+tool.scriptName = "script_mergeAndTrasnferToEOS_v6.sh"
+tool.xrdProtocol = "root://eoscms.cern.ch"
+
+# -- individual settings
+
+# # -- Z MC tree
+# tool.SEPath = "/pnfs/knu.ac.kr/data/cms/store/user/kplee/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8"
+
+# tool.crabDir = "crab_TnPTreeZ_v20190504_80X_DYJetsToLL_M50_MadgraphMLM"
+# tool.treeName = "TnPTreeZ_80X_DYJetsToLL_M50_MadgraphMLM.root"
+# tool.eosPath = "None"
+# tool.Register()
+
+# -- SingleMuon tree
+tool.SEPath = "/pnfs/knu.ac.kr/data/cms/store/user/kplee/SingleMuon"
+
+tool.crabDir = "crab_TnPTreeZ_v20190514_ignoreLocality_07Aug17_SingleMuon_Run2016D_GoldenJSON"
+tool.treeName = "TnPTreeZ_07Aug17_SingleMuon_Run2016D_GoldenJSON_Incomplete_20190607_Final.root"
+tool.eosPath = "None"
+tool.Register()
+
+
+tool.GenScript()
