8695cd8deb476ecb28ed8f7ccbd50577f4901a44
kplee <kplee@cern.ch>
2019-06-19 10:07:28 +0900

    move

--- CRAB/v06_UL_2017MC_HighStat/genScript_mergeAndTrasnferToEOS.py
+++ CRAB/v06_UL_2017MC_HighStat/genScript_mergeAndTrasnferToEOS.py
@@ -8,10 +8,10 @@ tool.xrdProtocol = "root://eoscms.cern.ch"
 # -- individual settings
 
 # # -- Z MC tree
-tool.SEPath = "/pnfs/knu.ac.kr/data/cms/store/user/kplee/RelValZMM_13"
+tool.SEPath = "/pnfs/knu.ac.kr/data/cms/store/user/kplee/RelValZMM_13UP17"
 
-tool.crabDir = "crab_TnPTreeZ_v20190613_1060For2018_RelValZMM"
-tool.treeName = "TnPTreeZ_1060For2018_RelValZMM.root"
+tool.crabDir = "crab_TnPTreeZ_v20190618_1060For2018_RelValZMM"
+tool.treeName = "TnPTreeZ_1060For2017_RelValZMM_HighStat.root"
 tool.eosPath = "None"
 tool.Register()
 
