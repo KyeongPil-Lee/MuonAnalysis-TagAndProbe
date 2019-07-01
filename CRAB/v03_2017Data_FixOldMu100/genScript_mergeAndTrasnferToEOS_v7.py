1709b4c38d52506260ba944517d6790f3e972c4d
kplee <kplee@cern.ch>
2019-07-01 14:02:21 +0900

    move tree with unprescaled IsoMu24

---
+++ CRAB/v03_2017Data_FixOldMu100/genScript_mergeAndTrasnferToEOS_v7.py
@@ -0,0 +1,49 @@
+from MuonAnalysis.TagAndProbe.TransferToEOSTool import *
+
+tool = TransferToEOSTool()
+# -- common settings
+tool.scriptName = "script_mergeAndTrasnferToEOS_v6.sh"
+tool.xrdProtocol = "root://eoscms.cern.ch"
+
+# -- individual settings
+
+# # -- Z MC tree
+# tool.SEPath = "/pnfs/knu.ac.kr/data/cms/store/user/kplee/DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8"
+
+# tool.crabDir = "crab_TnPTreeZ_v20190509_10FilesPerJob_94X_DYJetsToLL_M50_MadgraphMLM"
+# tool.treeName = "TnPTreeZ_94X_DYJetsToLL_M50_MadgraphMLM_Incomplete_20190607_Final.root"
+# tool.eosPath = "None"
+# tool.Register()
+
+# -- SingleMuon tree
+tool.SEPath = "/pnfs/knu.ac.kr/data/cms/store/user/kplee/SingleMuon"
+
+# tool.crabDir = "crab_TnPTreeZ_v20190607_notFinishedLumis_17Nov2017_SingleMuon_Run2017Bv1_GoldenJSON"
+# tool.treeName = "TnPTreeZ_17Nov2017_SingleMuon_Run2017Bv1_GoldenJSON_notFinishedLumis.root"
+# tool.eosPath = "None"
+# tool.Register()
+
+
+# tool.crabDir = "crab_TnPTreeZ_v20190607_notFinishedLumis_17Nov2017_SingleMuon_Run2017Ev1_GoldenJSON"
+# tool.treeName = "TnPTreeZ_17Nov2017_SingleMuon_Run2017Ev1_GoldenJSON_notFinishedLumis.root"
+# tool.eosPath = "None"
+# tool.Register()
+
+
+tool.crabDir = "crab_TnPTreeZ_v20190628_UnprescaledIsoMu24_17Nov2017_SingleMuon_Run2017Cv1_GoldenJSON"
+tool.treeName = "TnPTreeZ_17Nov2017_SingleMuon_Run2017Cv1_GoldenJSON_UnpreIsoMu24_Incomplete_20190701.root"
+tool.eosPath = "None"
+tool.Register()
+
+tool.crabDir = "crab_TnPTreeZ_v20190628_UnprescaledIsoMu24_17Nov2017_SingleMuon_Run2017Ev1_GoldenJSON"
+tool.treeName = "TnPTreeZ_17Nov2017_SingleMuon_Run2017Ev1_GoldenJSON_UnpreIsoMu24_Incomplete_20190701.root"
+tool.eosPath = "None"
+tool.Register()
+
+tool.crabDir = "crab_TnPTreeZ_v20190628_UnprescaledIsoMu24_17Nov2017_SingleMuon_Run2017Fv1_GoldenJSON"
+tool.treeName = "TnPTreeZ_17Nov2017_SingleMuon_Run2017Fv1_GoldenJSON_UnpreIsoMu24_Incomplete_20190701.root"
+tool.eosPath = "None"
+tool.Register()
+
+
+tool.GenScript()
