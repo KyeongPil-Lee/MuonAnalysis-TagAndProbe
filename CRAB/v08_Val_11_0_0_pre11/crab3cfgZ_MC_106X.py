eefa2abc6832cb411e97654b11c3d20fdbdd28d4
kplee <kplee@cern.ch>
2019-12-02 16:30:31 +0900

    ready for 11_0_0_pre11 validation

---
+++ CRAB/v08_Val_11_0_0_pre11/crab3cfgZ_MC_106X.py
@@ -0,0 +1,42 @@
+from CRABClient.UserUtilities import config, getUsernameFromSiteDB
+config = config()
+
+config.General.requestName = ''
+config.General.workArea = 'CRABDir'
+
+config.JobType.pluginName = 'Analysis'
+# config.JobType.psetName = '../../test/zmumu/tp_from_aod_MC_102X_v15.py'
+
+config.Data.inputDataset = ''
+
+config.Data.inputDBS = 'global'
+# config.Data.splitting = 'Automatic'
+config.Data.splitting = 'FileBased'
+config.Data.unitsPerJob = 1
+config.Data.outLFNDirBase = '/store/user/%s/' % (getUsernameFromSiteDB())
+config.Data.publication = False
+
+# config.Site.storageSite = 'T3_KR_KISTI'
+config.Site.storageSite = 'T2_KR_KNU'
+
+# config.Data.lumiMask = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions16/13TeV/ReReco/Final/Cert_271036-284044_13TeV_23Sep2016ReReco_Collisions16_JSON.txt'
+# FirstRun = 271036
+# LastRun = 284044
+# config.Data.runRange = '%d-%d' % (FirstRun, LastRun)
+
+
+version = '_v20191202_'
+# 'MultiCRAB' part
+if __name__ == '__main__':
+    
+    from CRABAPI.RawCommand import crabCommand
+
+    config.General.requestName = 'TnPTreeZ'+version+'RelValZMM_14_106X'
+    config.Data.inputDataset = '/RelValZMM_14/CMSSW_10_6_1_patch1-106X_mcRun3_2021_realistic_v3_rsb-v1/GEN-SIM-RECO'
+    config.JobType.psetName = '../../test/zmumu/tp_from_aod_MC_106X_mcRun3_2021_realistic_v3.py'
+    # config.Data.allowNonValidInputDataset = True # -- current status = Production
+    crabCommand('submit', config = config)
+
+    # config.General.requestName = ''
+    # config.Data.inputDataset = ''
+    # crabCommand('submit', config = config)
