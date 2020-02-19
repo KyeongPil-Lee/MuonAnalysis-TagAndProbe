3455545a5412ffcec7899f50247e5f4b5274a638
kplee <kplee@cern.ch>
2020-02-19 16:46:05 +0900

    add wrong sample

--- CRAB/v09_Val_UL16/script_transferTree_KNU_to_MUON.sh
+++ CRAB/v09_Val_UL16/script_transferTree_KNU_to_MUON.sh
@@ -1,5 +1,7 @@
 #!bin/bash
 
+echo "**************"
+echo "Correct sample"
 rsync -avz --progress -e 'ssh -p 50001' \
 /u/user/kplee/SE_UserHome/SingleMuon/crab_TnPTreeZ_v20200219_RelValZMM_13UP16_UL16_HighStat/*/*/*.root \
 kplee@147.47.50.161:/scratch/kplee/TagProbe/TnPTree/2020
@@ -9,4 +11,17 @@ echo "transfer: finished"
 echo "hadd: start"
 ssh -p 50001 kplee@147.47.50.161 "hadd /scratch/kplee/TagProbe/TnPTree/2020/TnPTreeZ_RelValZMM_13UP16_UL16_HighStat.root /scratch/kplee/TagProbe/TnPTree/2020/*.root"
 echo "hadd: done"
+echo "**************"
 
+echo "**************"
+echo "Wrong sample"
+rsync -avz --progress -e 'ssh -p 50001' \
+/u/user/kplee/SE_UserHome/SingleMuon/crab_TnPTreeZ_v20200219_RelValZMM_13UP16_UL16_HighStat_WrongL1/*/*/*.root \
+kplee@147.47.50.161:/scratch/kplee/TagProbe/TnPTree/2020/UL16_WrongL1
+
+echo "transfer: finished"
+
+echo "hadd: start"
+ssh -p 50001 kplee@147.47.50.161 "hadd /scratch/kplee/TagProbe/TnPTree/2020/TnPTreeZ_RelValZMM_13UP16_UL16_HighStat_WrongL1.root /scratch/kplee/TagProbe/TnPTree/2020/UL16_WrongL1/*.root"
+echo "hadd: done"
+echo "**************"
