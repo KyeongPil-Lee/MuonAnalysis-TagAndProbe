f077e4f0d6d8e761c6cfc53f6fdc188cd017f382
Kyeongpil Lee <kplee@cern.ch>
2020-03-31 17:42:01 +0900

    make the code for general transferring

--- CRAB/v12_Val_UL16_v2/pyscript_transfer.py
+++ CRAB/v12_Val_UL16_v2/pyscript_transfer.py
@@ -2,10 +2,31 @@ from MuonAnalysis.TagAndProbe.ROOTFileTransfer import *
 
 tool = TransferTool()
 tool.user = 'kplee'
-tool.host = '147.47.242.67'
-tool.port = 1240
+tool.host = '147.47.50.161'
+tool.port = 50001
+tool.do_hadd = True
+
+path_preVFP  = GetCRABOutputDir("crab_TnPTreeZ_v20200331_RelValZMM_13UP16_UL16_HighStat_L1Fix_preFVP")
+path_postVFP = GetCRABOutputDir("crab_TnPTreeZ_v20200331_RelValZMM_13UP16_UL16_HighStat_L1Fix_postFVP")
+
+tool.dic_input_to_mergedFileName = {
+    path_preVFP  : "TnPTreeZ_RelValZMM_13UP16_UL16_HighStat_L1FixMarch_preVFP.root",
+    path_postVFP : "TnPTreeZ_RelValZMM_13UP16_UL16_HighStat_L1FixMarch_postVFP.root",
+}
 
 tool.dic_input_to_outputPath = {
-    GetCRABOutputDir("crab_TnPTreeZ_v20200331_RelValZMM_13UP16_UL16_HighStat_L1Fix_preFVP") : "/home/kplee/Temp"
+    path_preVFP  : "/scratch/kplee/TagProbe/TnPTree/2020",
+    path_postVFP : "/scratch/kplee/TagProbe/TnPTree/2020",
 }
 tool.Transfer()
+
+
+# -- test
+# tool.dic_input_to_mergedFileName = {
+#     '/pnfs/knu.ac.kr/data/cms/store/user/kplee/RelValZMM_13UP16/crab_TnPTreeZ_v20200331_RelValZMM_13UP16_UL16_HighStat_L1Fix_preFVP' : "TnPTreeZ_RelValZMM_13UP16_UL16_HighStat_L1FixMarch_preVFP.root",
+# }
+
+# tool.dic_input_to_outputPath = {
+#     '/pnfs/knu.ac.kr/data/cms/store/user/kplee/RelValZMM_13UP16/crab_TnPTreeZ_v20200331_RelValZMM_13UP16_UL16_HighStat_L1Fix_preFVP' : "/home/kplee/Temp/preVFP",
+# }
+# tool.Transfer()
--- python/ROOTFileTransfer.py
+++ python/ROOTFileTransfer.py
@@ -1,10 +1,17 @@
-import os, sys
+import os, sys, getpass
+
+# -- in case paramiko is not available: pip install paramiko --user
+import paramiko 
+paramiko.util.log_to_file('./sftp.log')
 
 def GetCRABOutputDir(crabDirName, SEPath='/pnfs/knu.ac.kr/data/cms/store/user/kplee'):
     thePath = ""
-    for (path, dirName, list_file) in os.walk(SEPath):
-        if crabDirName == dirName:
-            thePath = "%s/%s" % (path, dirName)
+    print "[GetCRABOutputDir] Searching for %s under %s ..." % (crabDirName, SEPath)
+
+    for (path, list_dir, list_file) in os.walk(SEPath):
+        # print "current directories: %s" % list_dir
+        if crabDirName in list_dir:
+            thePath = "%s/%s" % (path, crabDirName)
             break
 
     if thePath == "":
@@ -13,11 +20,13 @@ def GetCRABOutputDir(crabDirName, SEPath='/pnfs/knu.ac.kr/data/cms/store/user/kp
 
     if len(os.listdir(thePath)) > 1:
         print "[GetCRABOutputDir] Find more than 1 sub-directories under the crab directory"
-        print "  crabDir = %s" % crabDir
+        print "  crabDir = %s" % crabDirName
         print "  listDir = ", os.listdir(thePath)
         sys.exit()
 
-    return thePath 
+    print "[GetCRABOutputDir] Find: %s" % thePath
+
+    return thePath
 
 
 class TransferTool:
@@ -29,32 +38,124 @@ class TransferTool:
         self.port = 22
         
         self.do_hadd = False
-        self.needPassward = False
+        self.dic_input_to_mergedFileName = {}
 
         # -- internal variables
         self.list_outputFile = []
-
+        self.sftp = ""
 
     def Transfer(self):
-        for inputPath in dic_input_to_outputPath.keys():
-            outputPath = dic_input_to_outputPath[inputPath]
+        transport = paramiko.Transport((self.host, self.port))
+        password = getpass.getpass(prompt="password for %s@%s: " % (self.user, self.host) )
+        try:
+            transport.connect(username = self.user, password = password)
+        except paramiko.ssh_exception.AuthenticationException:
+            print "[TransferTool] Wrong password!"
+            sys.exit()
+
+        # -- open SFTP
+        self.sftp = paramiko.SFTPClient.from_transport(transport)
+
+        for inputPath in self.dic_input_to_outputPath.keys():
+            outputPath = self.dic_input_to_outputPath[inputPath]
             self.Transfer_Input_to_Output(inputPath, outputPath)
+            print "[Transfer] Transferring is done"
+            print "   %s ---> %s" % (inputPath, outputPath)
 
 
     def Transfer_Input_to_Output(self, inputPath, outputPath):
+        if not self.SFTP_exists(outputPath):
+            print "%s does not exist in %s: make it" % (outputPath, self.host)
+            self.mkdir_p(outputPath)
+
         list_outputFile = self.GetListOfOutputFiles(inputPath)
-        print list_outputFile
+        print "list_outputFile = ", list_outputFile
+
+        if self.do_hadd:
+            mergedFilePath = self.MergeROOTFiles(inputPath, list_outputFile)
+            list_outputFile = [mergedFilePath] # -- transfer merged file only
+
+        for outputFile in list_outputFile:
+            fileName = outputFile.split("/")[-1]
+            self.sftp.put(outputFile, outputPath+"/"+fileName)
+
 
 
     # -- search .root file under the drectory (including sub-directories)
     def GetListOfOutputFiles(self, inputPath):
         list_outputFile = []
-        for (path, dirName, list_file) in os.walk(inputPath):
+        print "[GetListOfOutputFiles] Collecting files from %s ..." % inputPath
+
+        for (path, list_dir, list_file) in os.walk(inputPath):
             for fileName in list_file:
-                if ".root" == os.path.splittext(fileName) and "failed" not in path2:
+                # print "path = ", path
+                # print "list_dir = ", list_dir
+                # print "list_file = ", list_file
+                # print "fileName = ", fileName
+                # print "os.path.splitext = ", os.path.splitext(fileName)
+
+                if ".root" == os.path.splitext(fileName)[-1] and "failed" not in path:
                     list_outputFile.append( "%s/%s" % (path, fileName) )
 
         return list_outputFile
+
+    def MergeROOTFiles(self, inputPath, list_file):
+        if len(self.dic_input_to_mergedFileName) == 0:
+            print "[MergeROOTFiles] dic_input_to_mergedFileName is empty!"
+            sys.exit()
+
+        mergedFileName = self.dic_input_to_mergedFileName[inputPath]
+
+        # -- save in current directory
+        cwd = os.getcwd()
+        fulloutputPath = "%s/%s" % (cwd, mergedFileName)
+
+        if os.path.exists(fulloutputPath):
+            print "[MergeROOTFiles] already merged file exists: %s" % fulloutputPath
+            return fulloutputPath
+
+        cmd_hadd = "hadd %s \\\n" % (fulloutputPath)
+        for fileName in list_file:
+            if fileName == list_file[-1]:
+                cmd_hadd = cmd_hadd + "%s" % fileName
+            else:
+                cmd_hadd = cmd_hadd + "%s \\\n" % fileName
+
+        print cmd_hadd
+        os.system(cmd_hadd)
+
+        return fulloutputPath
+
+    def SFTP_exists(self, path):
+        try:
+            self.sftp.stat(path)
+        except IOError, e:
+            if e[0] == 2: # -- IOError: [Errno 2] No such file
+                return False
+            raise
+        else:
+            return True
+
+    # -- https://stackoverflow.com/questions/14819681/upload-files-using-sftp-in-python-but-create-directories-if-path-doesnt-exist
+    def mkdir_p(self, remote_directory):
+        if remote_directory == '/':
+            # absolute path so change directory to root
+            self.sftp.chdir('/')
+            return
+        if remote_directory == '':
+            # top-level relative directory must exist
+            return
+        try:
+            self.sftp.chdir(remote_directory) # sub-directory exists
+        except IOError:
+            dirname, basename = os.path.split(remote_directory.rstrip('/'))
+            self.mkdir_p(dirname) # make parent directories
+            self.sftp.mkdir(basename) # sub-directory missing, so created it
+            self.sftp.chdir(basename)
+            return True
+
+
+
 
 
 
