606cbdf4a8b37800f497cb6de018623f5ba51c22
kplee <kplee@cern.ch>
2020-04-04 17:05:15 +0900

    trasnfer SingleMuon data

---
+++ CRAB/v11_UL_2018DataMC/pyscript_transfer.py
@@ -0,0 +1,29 @@
+from MuonAnalysis.TagAndProbe.ROOTFileTransfer import *
+
+tool = TransferTool()
+tool.user = 'kplee'
+tool.host = 'lxplus.cern.ch'
+tool.port = 22
+tool.do_hadd = False
+
+tool.dic_input_to_outputPath = {
+    GetCRABOutputDir("crab_TnPTreeZ_v20200324_12Nov2019_SingleMuon_Run2018A_GoldenJSON")  : "/eos/cms/store/group/phys_muon/TagAndProbe/ULRereco/2018/102X/SingleMuon_Run2018A_98Percent",
+    GetCRABOutputDir("crab_TnPTreeZ_v20200324_12Nov2019_SingleMuon_Run2018B_GoldenJSON") : "/eos/cms/store/group/phys_muon/TagAndProbe/ULRereco/2018/102X/SingleMuon_Run2018B_80Percent",
+}
+
+tool.Transfer()
+
+
+# path_preVFP  = GetCRABOutputDir("crab_TnPTreeZ_v20200324_12Nov2019_SingleMuon_Run2018A_GoldenJSON")
+# path_postVFP = GetCRABOutputDir("crab_TnPTreeZ_v20200324_12Nov2019_SingleMuon_Run2018B_GoldenJSON")
+
+# tool.dic_input_to_mergedFileName = {
+#     path_preVFP  : "dummay.root",
+#     path_postVFP : "TnPTreeZ_RelValZMM_13UP16_UL16_HighStat_L1FixMarch_postVFP.root",
+# }
+
+# tool.dic_input_to_outputPath = {
+#     path_preVFP  : "/scratch/kplee/TagProbe/TnPTree/2020",
+#     path_postVFP : "/scratch/kplee/TagProbe/TnPTree/2020",
+# }
+# tool.Transfer()
