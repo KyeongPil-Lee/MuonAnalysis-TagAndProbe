cf33e8d98884bb604df4746864e00c96f965e80f
kplee <kplee@cern.ch>
2020-04-10 13:10:41 +0900

    move remaining jobs

--- CRAB/v11_UL_2018DataMC/pyscript_transfer.py
+++ CRAB/v11_UL_2018DataMC/pyscript_transfer.py
@@ -11,8 +11,10 @@ tool.dic_input_to_outputPath = {
     # GetCRABOutputDir("crab_TnPTreeZ_v20200324_12Nov2019_SingleMuon_Run2018B_GoldenJSON") : "/eos/cms/store/group/phys_muon/TagAndProbe/ULRereco/2018/102X/SingleMuon_Run2018B_80Percent",
     # "/pnfs/knu.ac.kr/data/cms/store/user/kplee/SingleMuon/crab_TnPTreeZ_v20200324_12Nov2019_SingleMuon_Run2018D_GoldenJSON" : "/eos/cms/store/group/phys_muon/TagAndProbe/ULRereco/2018/102X/SingleMuon_Run2018D_98Percent",
 
-    "/u/user/kplee/SE_UserHome/SingleMuon/crab_TnPTreeZ_v20200404_12Nov2019_SingleMuon_Run2018B_GoldenJSON" : "/eos/cms/store/group/phys_muon/TagAndProbe/ULRereco/2018/102X/SingleMuon_Run2018B_80Percent",
-}
+    # "/u/user/kplee/SE_UserHome/SingleMuon/crab_TnPTreeZ_v20200404_12Nov2019_SingleMuon_Run2018B_GoldenJSON" : "/eos/cms/store/group/phys_muon/TagAndProbe/ULRereco/2018/102X/SingleMuon_Run2018B_80Percent",
+
+    "/u/user/kplee/SE_UserHome/SingleMuon/crab_TnPTreeZ_v20200404_12Nov2019_SingleMuon_Run2018A_GoldenJSON" : "/eos/cms/store/group/phys_muon/TagAndProbe/ULRereco/2018/102X/SingleMuon_Run2018A_98Percent",
+    "/u/user/kplee/SE_UserHome/SingleMuon/crab_TnPTreeZ_v20200406_12Nov2019_SingleMuon_Run2018D_GoldenJSON" : "/eos/cms/store/group/phys_muon/TagAndProbe/ULRereco/2018/102X/SingleMuon_Run2018D_98Percent",}
 
 tool.Transfer()
 
