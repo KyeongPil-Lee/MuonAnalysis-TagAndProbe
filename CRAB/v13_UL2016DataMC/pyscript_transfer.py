5492f984aefbe39b9b5486e89ce239ea2f4131cd
Kyeongpil Lee <kplee@cern.ch>
2020-06-17 14:00:20 +0900

    updated script

--- CRAB/v13_UL2016DataMC/pyscript_transfer.py
+++ CRAB/v13_UL2016DataMC/pyscript_transfer.py
@@ -2,21 +2,22 @@ from MuonAnalysis.TagAndProbe.ROOTFileTransfer import *
 
 tool = TransferTool()
 tool.user = 'kplee'
-tool.host = 'lxplus.cern.ch'
+# tool.host = 'lxplus.cern.ch' # -- Connection reset by peer error
+tool.host = 'lxplus701.cern.ch'
 tool.port = 22
 tool.do_hadd = False
 
 tool.dic_input_to_outputPath = {
-    "/u/user/kplee/SE_UserHome/SingleMuon/crab_TnPTreeZ_v20200609_21Feb2020_SingleMuon_Run2016Bver2_HIPM_GoldenJSON" : "/eos/cms/store/group/phys_muon/TagAndProbe/ULRereco/2016/102X/SingleMuon_Run2016Bver2_HIPM",
-    "/u/user/kplee/SE_UserHome/SingleMuon/crab_TnPTreeZ_v20200609_21Feb2020_SingleMuon_Run2016C_HIPM_GoldenJSON"     : "/eos/cms/store/group/phys_muon/TagAndProbe/ULRereco/2016/102X/SingleMuon_Run2016C_HIPM",
-    "/u/user/kplee/SE_UserHome/SingleMuon/crab_TnPTreeZ_v20200609_21Feb2020_SingleMuon_Run2016D_HIPM_GoldenJSON"     : "/eos/cms/store/group/phys_muon/TagAndProbe/ULRereco/2016/102X/SingleMuon_Run2016D_HIPM",
-    "/u/user/kplee/SE_UserHome/SingleMuon/crab_TnPTreeZ_v20200609_21Feb2020_SingleMuon_Run2016E_HIPM_GoldenJSON"     : "/eos/cms/store/group/phys_muon/TagAndProbe/ULRereco/2016/102X/SingleMuon_Run2016E_HIPM",
+    "/u/user/kplee/SE_UserHome/SingleMuon/crab_TnPTreeZ_v20200610_21Feb2020_SingleMuon_Run2016Bver2_HIPM_GoldenJSON" : "/eos/cms/store/group/phys_muon/TagAndProbe/ULRereco/2016/102X/SingleMuon_Run2016Bver2_HIPM",
+    # "/u/user/kplee/SE_UserHome/SingleMuon/crab_TnPTreeZ_v20200609_21Feb2020_SingleMuon_Run2016C_HIPM_GoldenJSON"     : "/eos/cms/store/group/phys_muon/TagAndProbe/ULRereco/2016/102X/SingleMuon_Run2016C_HIPM",
+    # "/u/user/kplee/SE_UserHome/SingleMuon/crab_TnPTreeZ_v20200609_21Feb2020_SingleMuon_Run2016D_HIPM_GoldenJSON"     : "/eos/cms/store/group/phys_muon/TagAndProbe/ULRereco/2016/102X/SingleMuon_Run2016D_HIPM",
+    # "/u/user/kplee/SE_UserHome/SingleMuon/crab_TnPTreeZ_v20200609_21Feb2020_SingleMuon_Run2016E_HIPM_GoldenJSON"     : "/eos/cms/store/group/phys_muon/TagAndProbe/ULRereco/2016/102X/SingleMuon_Run2016E_HIPM",
     "/u/user/kplee/SE_UserHome/SingleMuon/crab_TnPTreeZ_v20200609_21Feb2020_SingleMuon_Run2016F_HIPM_GoldenJSON"     : "/eos/cms/store/group/phys_muon/TagAndProbe/ULRereco/2016/102X/SingleMuon_Run2016F_HIPM",
-    "/u/user/kplee/SE_UserHome/SingleMuon/crab_TnPTreeZ_v20200609_21Feb2020_SingleMuon_Run2016F_GoldenJSON"          : "/eos/cms/store/group/phys_muon/TagAndProbe/ULRereco/2016/102X/SingleMuon_Run2016F",
-    "/u/user/kplee/SE_UserHome/SingleMuon/crab_TnPTreeZ_v20200609_21Feb2020_SingleMuon_Run2016G_GoldenJSON"          : "/eos/cms/store/group/phys_muon/TagAndProbe/ULRereco/2016/102X/SingleMuon_Run2016G",
-    "/u/user/kplee/SE_UserHome/SingleMuon/crab_TnPTreeZ_v20200609_21Feb2020_SingleMuon_Run2016H_GoldenJSON"          : "/eos/cms/store/group/phys_muon/TagAndProbe/ULRereco/2016/102X/SingleMuon_Run2016H",
+    # "/u/user/kplee/SE_UserHome/SingleMuon/crab_TnPTreeZ_v20200609_21Feb2020_SingleMuon_Run2016F_GoldenJSON"          : "/eos/cms/store/group/phys_muon/TagAndProbe/ULRereco/2016/102X/SingleMuon_Run2016F",
+    # "/u/user/kplee/SE_UserHome/SingleMuon/crab_TnPTreeZ_v20200609_21Feb2020_SingleMuon_Run2016G_GoldenJSON"          : "/eos/cms/store/group/phys_muon/TagAndProbe/ULRereco/2016/102X/SingleMuon_Run2016G",
+    # "/u/user/kplee/SE_UserHome/SingleMuon/crab_TnPTreeZ_v20200609_21Feb2020_SingleMuon_Run2016H_GoldenJSON"          : "/eos/cms/store/group/phys_muon/TagAndProbe/ULRereco/2016/102X/SingleMuon_Run2016H",
 
-    '/u/user/kplee/SE_UserHome/DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8/crab_TnPTreeZ_v20200609_UL16MC_DYJetsToLL_M50_Madgraph' : "/eos/cms/store/group/phys_muon/TagAndProbe/ULRereco/2016/102X/DY_M50_Madgraph",
+    # '/u/user/kplee/SE_UserHome/DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8/crab_TnPTreeZ_v20200609_UL16MC_DYJetsToLL_M50_Madgraph' : "/eos/cms/store/group/phys_muon/TagAndProbe/ULRereco/2016/102X/DY_M50_Madgraph",
 
     # GetCRABOutputDir("crab_TnPTreeZ_v20200324_12Nov2019_SingleMuon_Run2018A_GoldenJSON")  : "/eos/cms/store/group/phys_muon/TagAndProbe/ULRereco/2018/102X/SingleMuon_Run2018A_98Percent",
     # GetCRABOutputDir("crab_TnPTreeZ_v20200324_12Nov2019_SingleMuon_Run2018B_GoldenJSON") : "/eos/cms/store/group/phys_muon/TagAndProbe/ULRereco/2018/102X/SingleMuon_Run2018B_80Percent",
